a9b08946082f5dd1d196481759814909caf47825
Nycto <James@RoundEights.com>
2013-03-23 14:22:34 -0700

    Add string-replace grunt task

--- grunt.js
+++ grunt.js
@@ -1,6 +1,8 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
+    grunt.loadNpmTasks('grunt-string-replace');
+
     // Project configuration.
     grunt.initConfig({
         pkg: '<json:package.json>',
@@ -13,9 +15,29 @@ module.exports = function(grunt) {
                 dest: 'dist/picoModal.concat.js'
             }
         },
+        "string-replace": {
+            dist: {
+                src: [ '<config:concat.dist.dest>' ],
+                dest: 'dist/picoModal.replaced.js',
+                options: {
+                    replacements: [
+                        { pattern: /\bwatch\b/g, replacement: "w" },
+                        { pattern: /\btrigger\b/g, replacement: "t" },
+                        { pattern: /\belem\b/g, replacement: "e" },
+                        { pattern: /\bchild\b/g, replacement: "c" },
+                        { pattern: /\bstylize\b/g, replacement: "s" },
+                        { pattern: /\bclazz\b/g, replacement: "z" },
+                        { pattern: /\bhtml\b/g, replacement: "h" },
+                        { pattern: /\bgetWidth\b/g, replacement: "d" },
+                        { pattern: /\bonClick\b/g, replacement: "o" },
+                        { pattern: /\bdestroy\b/g, replacement: "x" }
+                    ]
+                }
+            }
+        },
         min: {
             dist: {
-                src: ['<config:concat.dist.dest>'],
+                src: ['<config:string-replace.dist.dest>'],
                 dest: 'dist/picoModal.min.js'
             }
         },
@@ -28,6 +50,6 @@ module.exports = function(grunt) {
     });
 
     // Default task.
-    grunt.registerTask('default', 'lint concat min');
+    grunt.registerTask('default', 'lint concat string-replace min');
 
 };
