57d6caf42d92bbad4ef32560623e524e0272d4ad
Nycto <James@RoundEights.com>
2014-06-29 15:13:28 -0700

    Remove string-replace from build
    This was mucking with the destroy method.

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,37 +30,16 @@ module.exports = function(grunt) {
             }
         },
 
-        "string-replace": {
-            build: {
-                src: [ 'src/picoModal.js' ],
-                dest: 'build/picoModal.replaced.js',
-                options: {
-                    replacements: [
-                        { pattern: /\bwatch\b/g, replacement: "w" },
-                        { pattern: /\btrigger\b/g, replacement: "t" },
-                        { pattern: /\belem\b/g, replacement: "e" },
-                        { pattern: /\bchild\b/g, replacement: "c" },
-                        { pattern: /\bstylize\b/g, replacement: "s" },
-                        { pattern: /\bclazz\b/g, replacement: "z" },
-                        { pattern: /\bhtml\b/g, replacement: "h" },
-                        { pattern: /\bgetWidth\b/g, replacement: "d" },
-                        { pattern: /\bonClick\b/g, replacement: "o" },
-                        { pattern: /\bdestroy\b/g, replacement: "x" }
-                    ]
-                }
-            }
-        },
-
         uglify: {
             build: {
-                src: 'build/picoModal.replaced.js',
+                src: 'src/picoModal.js',
                 dest: 'build/picoModal-<%= pkg.version %>.min.js'
             }
         },
 
         watch: {
             files: ['<%= jshint.files %>'],
-            tasks: ['jshint']
+            tasks: ['default']
         },
 
         connect: {
@@ -75,13 +54,12 @@ module.exports = function(grunt) {
 
     // Plugins
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-string-replace');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-connect');
 
     // Default task(s).
-    grunt.registerTask('default', ['jshint', 'string-replace', 'uglify']);
+    grunt.registerTask('default', ['jshint', 'uglify']);
 
     grunt.registerTask('dev', ['default', 'connect', 'watch']);
 };
