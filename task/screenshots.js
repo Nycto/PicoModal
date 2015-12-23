83cd3b3449f5ffad9e1d1682011bc2df746e1813
Nycto <James@RoundEights.com>
2015-12-23 15:02:13 -0800

    Close tunnels on SIGINT

--- task/screenshots.js
+++ task/screenshots.js
@@ -48,21 +48,36 @@ module.exports = function ( grunt, options ) {
             }
         });
 
+        // Will store a future that is fulfilled when the tunnel is stopped
+        var stopped;
+
+        /** Stops the tunnel */
+        function stopTunnel() {
+            if ( !stopped ) {
+                var stopping = Q.defer();
+
+                grunt.log.writeln('=> Closing Tunnel'.inverse.bold);
+                tunnel.stop(function () {
+                    grunt.log.ok('Tunnel Closed');
+                    stopping.resolve();
+                });
+
+                stopped = stopping.promise
+                    .timeout(60000, "Timed out trying to close tunnel");
+            }
+
+            return stopped;
+        }
+
+        // Ensure this tunnel is cleaned up if a process is killed
+        require('cleankill').onInterrupt(function (done) {
+            stopTunnel().fin(done);
+        });
+
         return defer.promise
             .timeout(60000, "Timed out trying to create tunnel")
             .then(function () {
-                return withTunnel(tunnel).fin(function () {
-                    var stopping = Q.defer();
-
-                    grunt.log.writeln('=> Closing Tunnel'.inverse.bold);
-                    tunnel.stop(function () {
-                        grunt.log.ok('Tunnel Closed');
-                        stopping.resolve();
-                    });
-
-                    return stopping.promise
-                        .timeout(5000, "Timed out trying to close tunnel");
-                });
+                return withTunnel(tunnel).fin(stopTunnel);
             });
     }
 
