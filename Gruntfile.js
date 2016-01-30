9a7da1d58ac04e684a65c1c662b5b2c0dbb38f8b
Nycto <James@RoundEights.com>
2016-01-29 17:01:33 -0800

    Group saucelab browsers

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,72 +1,84 @@
 /* global module: false, process: false */
 
 // Browsers to test on saucelabs
-var browsers = [
-    {
-        browserName: 'firefox',
-        platform: 'WIN8.1'
-    },
-    {
-        browserName: 'chrome',
-        platform: 'windows 8.1'
-    },
-    {
-        browserName: 'internet explorer',
-        version: '11',
-        platform: 'WIN8.1'
-    },
-    {
-        browserName: 'internet explorer',
-        version: '10',
-        platform: 'WIN8'
-    },
-    {
-        browserName: 'internet explorer',
-        version: '9',
-        platform: 'WIN7'
-    },
-    {
-        browserName: 'android',
-        platform: 'Linux',
-        version: '4.3',
-        deviceName: 'Android Emulator'
-    },
-    {
-        browserName: 'android',
-        platform: 'Linux',
-        version: '4.4',
-        deviceName: 'Google Nexus 7 HD Emulator'
-    },
-    {
-        browserName: 'android',
-        platform: 'Linux',
-        version: '5.0',
-        deviceName: 'Android Emulator'
-    },
-    {
-        browserName: 'android',
-        platform: 'Linux',
-        version: '5.1',
-        deviceName: 'Android Emulator'
-    },
-    {
-        browserName: 'iphone',
-        platform: 'OS X 10.10',
-        version: '9.2',
-        deviceName: 'iPhone Simulator'
-    },
-    {
-        browserName: 'iphone',
-        platform: 'OS X 10.10',
-        version: '8.4',
-        deviceName: 'iPhone Simulator'
-    },
-    {
-        browserName: 'safari',
-        platform: 'OS X 10.11',
-        version: '9.0'
-    }
-];
+var browsers = {
+    firefox: [
+        {
+            browserName: 'firefox',
+            platform: 'WIN8.1'
+        }
+    ],
+    chrome: [
+        {
+            browserName: 'chrome',
+            platform: 'windows 8.1'
+        }
+    ],
+    ie:[
+        {
+            browserName: 'internet explorer',
+            version: '11',
+            platform: 'WIN8.1'
+        },
+        {
+            browserName: 'internet explorer',
+            version: '10',
+            platform: 'WIN8'
+        },
+        {
+            browserName: 'internet explorer',
+            version: '9',
+            platform: 'WIN7'
+        }
+    ],
+    android: [
+        {
+            browserName: 'android',
+            platform: 'Linux',
+            version: '4.3',
+            deviceName: 'Android Emulator'
+        },
+        {
+            browserName: 'android',
+            platform: 'Linux',
+            version: '4.4',
+            deviceName: 'Google Nexus 7 HD Emulator'
+        },
+        {
+            browserName: 'android',
+            platform: 'Linux',
+            version: '5.0',
+            deviceName: 'Android Emulator'
+        },
+        {
+            browserName: 'android',
+            platform: 'Linux',
+            version: '5.1',
+            deviceName: 'Android Emulator'
+        }
+    ],
+    ios: [
+        {
+            browserName: 'iphone',
+            platform: 'OS X 10.10',
+            version: '9.2',
+            deviceName: 'iPhone Simulator'
+        },
+        {
+            browserName: 'iphone',
+            platform: 'OS X 10.10',
+            version: '8.4',
+            deviceName: 'iPhone Simulator'
+        }
+    ],
+    safari: [
+        {
+            browserName: 'safari',
+            platform: 'OS X 10.11',
+            version: '9.0'
+        }
+    ]
+};
 
 module.exports = function(grunt) {
     "use strict";
@@ -123,21 +135,33 @@ module.exports = function(grunt) {
             files: [ "test/*.js" ]
         },
 
-        'saucelabs-custom': {
-            all: {
-                options: {
-                    urls: [ 'http://localhost:8080' ],
-                    build: process.env.CI_BUILD_NUMBER || Date.now(),
-                    testname: 'PicoModal unit tests',
-                    public: "public",
-                    pollInterval: 5000,
-                    statusCheckAttempts: 48,
-                    'max-duration': 180,
-                    maxRetries: 1,
-                    browsers: browsers
+        // Register saucelabs configuration for each browser group. This allows
+        // them to be run individually instead of all or nothing
+        'saucelabs-custom': Object.keys(browsers).reduce(
+            function (conf, key) {
+                var clone = Object.create(conf.all.options);
+                clone.browsers = browsers[key];
+                conf[key] = { options: clone };
+                return conf;
+            },
+            {
+                all: {
+                    options: {
+                        urls: [ 'http://localhost:8080' ],
+                        build: process.env.CI_BUILD_NUMBER || Date.now(),
+                        testname: 'PicoModal unit tests',
+                        public: "public",
+                        pollInterval: 5000,
+                        statusCheckAttempts: 48,
+                        'max-duration': 180,
+                        maxRetries: 1,
+                        browsers: Object.keys(browsers)
+                            .map(function (key) { return browsers[key]; })
+                            .reduce(function (a, b) { return a.concat(b); }, [])
+                    }
                 }
             }
-        }
+        )
     });
 
     // Plugins
@@ -162,7 +186,14 @@ module.exports = function(grunt) {
 
     grunt.registerTask(
         'sauce',
-        ['default', 'domTest:server', 'saucelabs-custom']);
+        ['default', 'domTest:server', 'saucelabs-custom:all']);
+
+    // Register a grunt target for testing individual browser groups
+    Object.keys(browsers).forEach(function (browser) {
+        grunt.registerTask(
+            browser,
+            ['default', 'domTest:server', 'saucelabs-custom:' + browser]);
+    });
 
     // Pull requests don't have access to saucelabs, so we need
     // to disable that
